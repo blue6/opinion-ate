33d4ec767ee288849cb4fdcbdc86dfe46c3c4f64
blue6 <blue6@blue6-no-MacBook-Pro.local>
2021-11-20 23:24:32 +0900

    Add Restaurants PASS

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,6 +1,7 @@
 export const START_LOADING = 'START_LOADING';
 export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
 export const RECORD_LOADING_ERROR = 'RECORD_LOADING_ERROR';
+export const ADD_RESTAURANT = 'ADD_RESTAURANT';
 
 export const loadRestaurants = () => (dispatch, getState, api) => {
   dispatch(startLoading());
@@ -12,6 +13,12 @@ export const loadRestaurants = () => (dispatch, getState, api) => {
     .catch(() => {
       dispatch(recordLoadingError());
     });
+};
+
+export const createRestaurant = name => (dispatch, getState, api) => {
+  return api.createRestaurant(name).then(record => {
+    dispatch(addRestaurant(record));
+  });
 };
 
 const startLoading = () => ({
@@ -26,3 +33,5 @@ const storeRestaurants = records => ({
 const recordLoadingError = () => ({
   type: RECORD_LOADING_ERROR,
 });
+
+const addRestaurant = record => ({type: ADD_RESTAURANT, record});
